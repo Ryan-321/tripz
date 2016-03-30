10932dcf5164039f72667f399eeb0e69cfb08556
Ryan Kahle <jrkahle@hotmail.com>
2016-03-30 16:54:02 -0400

    functionality complete

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var map = L.map('map').setView([38.8949549, -77.0366455], 15);
+
+L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  maxZoom: 18,
+  // Replace the below properties with your `Map ID` and `Default Public Token` values you saved earlier, respectively.
+  id: 'rhinoman333.pd891lgo',
+  accessToken:'pk.eyJ1Ijoicmhpbm9tYW4zMzMiLCJhIjoiY2lscGxieGMxMDhqeHRya25vYjR4YXUycCJ9.Cj2wGJWi6IbEi1dkOWLWEA'
+}).addTo(map);
+
+var control = L.Routing.control({
+  waypoints: [null],
+  routeWhileDragging: true,
+}).addTo(map);
+
+var points = new Array;
+map.on("click", function( event ){
+  var lng_lat = new L.LatLng(event.latlng.lat, event.latlng.lng);
+  var marker = new L.Routing.Waypoint(lng_lat);
+  points.push(marker)
+  control.setWaypoints(points);
+});
+
+$("p.remove").on("click", function(e){
+  points.splice(points.length - 1, 1);
+  control.setWaypoints(points);
+});
+$("p.distance").on("click", function(e){
+  var meters = control._routes[0].summary.totalDistance;
+  var miles = meters * 0.000621371192;
+  alert(miles.toFixed(2));
+});
+new L.Control.GeoSearch({
+  provider: new L.GeoSearch.Provider.Google(),
+  showMarker: false,
+  retainZoomLevel: false,
+}).addTo(map);
